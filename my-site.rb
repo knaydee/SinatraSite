2b0d98c51d790ca997981c9e41839b9b4a803b4e
Meighan Rasley <meighanr@gmail.com>
2015-10-27 14:26:51 -0700

    Finish Wave 1: Learn Sinatra

--- my-site.rb
+++ my-site.rb
@@ -2,7 +2,19 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
+  get "/index.html" do
+    erb :index
+  end
 
+  get "/about.html" do
+    erb :about
+  end
+
+  get "/blog.html" do
+    erb :blog
+  end
+
+  get "/projects.html" do
+    erb :projects
   end
 end
