23421cd8504513c2022ce73d50a289a9b637c480
Meighan Rasley <meighanr@gmail.com>
2015-10-30 10:42:51 -0700

    Add layout page, various new CSS styles

--- my-site.rb
+++ my-site.rb
@@ -3,22 +3,30 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
-    erb: index
+    erb :index
   end
-  
+
   get "/index.html" do
+    @h1 = "Welcome to Meighs' Page"
+    @h4 = "A website all about me, mine, and myself."
     erb :index
   end
 
   get "/about.html" do
+    @h1 = "About Meighan"
+    @h4 = "Here's some stuff you should know about me..."
     erb :about
   end
 
   get "/blog.html" do
+    @h1 = "Meighan's Blog"
+    @h4 = "A few things I have to say..."
     erb :blog
   end
 
   get "/projects.html" do
+    @h1 = "Meighan's Projects"
+    @h4 = "A few things I've been tinkering around with..."
     erb :projects
   end
 end
